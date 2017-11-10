0f1db1a99e39d993c66e94bf0fb2ca9fa7ad507f
vivek10688 <er.vivek.it@gmail.com>
2017-11-10 17:27:07 +0530

    first commit

---
+++ app/products/products.factory.js
@@ -0,0 +1,69 @@
+app.factory("productsFactory", function($http){
+ 
+    var factory = {};
+ 
+    // read all products
+    factory.readProducts = function(){
+        return $http({
+            method: 'GET',
+            url: 'http://localhost/phpcurdapi/read.php'
+        });
+    };
+     
+    // create product
+factory.createProduct = function($scope){
+    return $http({
+        method: 'POST',
+        data: {
+            'name' : $scope.name,
+            'description' : $scope.description,
+            'price' : $scope.price,
+            'category_id' : 1
+        },
+        url: 'http://localhost/phpcurdapi/create.php'
+    });
+};
+// read one product
+factory.readOneProduct = function(id){
+    return $http({
+        method: 'GET',
+        url: 'http://localhost/phpcurdapi/read_one.php?id=' + id
+    });
+};
+ // update product
+factory.updateProduct = function($scope){
+ 
+    return $http({
+        method: 'POST',
+        data: {
+            'id' : $scope.id,
+            'name' : $scope.name,
+            'description' : $scope.description,
+            'price' : $scope.price,
+            'category_id' : 1
+        },
+        url: 'http://localhost/phpcurdapi/update.php'
+    });
+};
+
+ // delete product
+factory.deleteProduct = function(id){
+    return $http({
+        method: 'POST',
+        data: { 'id' : id },
+        url: 'http://localhost/phpcurdapi/delete.php'
+    });
+};
+ // search all products
+factory.searchProducts = function(keywords){
+    return $http({
+        method: 'GET',
+        url: 'http://localhost/phpcurdapi/search.php?s=' + keywords
+    });
+};
+// searchProducts will be here
+ 
+// deleteProduct will be here
+// updateProduct will be here
+    return factory;
+});
